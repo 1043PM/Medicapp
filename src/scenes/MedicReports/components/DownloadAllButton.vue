0cf7aa9683edb1d7926cf550877d064bfaee03d8
Marco Alejandro Chavez Santos <marcochavez1940@gmail.com>
2019-04-22 02:15:23 -0500

    done medic report page

---
+++ src/scenes/MedicReports/components/DownloadAllButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-btn v-show="selected.length >= 1" color="info" @click="downloadAllReports()">Descargar seleccionados</v-btn>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    selected: Array,
+    downloadAll: Function
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    downloadAllReports() {
+      this.downloadAll(this.selected);
+    }
+  }
+};
+</script>
---
+++ src/scenes/MedicReports/components/EditButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-btn flat icon id="button">
+      <v-icon>edit</v-icon>
+    </v-btn>
+  </v-layout>
+</template>
+<script>
+export default {
+  props: {
+    folio: String
+  },
+  data: () => ({})
+};
+</script>
+
+<style scoped>
+#button {
+  margin: 0px;
+}
+</style>
