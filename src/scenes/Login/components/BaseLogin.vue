0bd6c5ac79f2c1e755f31ee824b132f6a0809a32
Alex <alejandro.sanchez.queb@gmail.com>
2019-04-22 23:32:21 -0500

    Login form and scene
    not ready yet

---
+++ src/scenes/Login/components/BaseLogin.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat color="blue-grey lighten-5" class>
+    <v-card-title class="title">{{title}}</v-card-title>
+    <v-card-text>
+      Bienvenido a la pagina de Servicios Medicos Profesionales para el transporte
+      <v-alert color="red" v-show="Boolean(error)">{{error}}</v-alert>
+      <v-form>    
+        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+      </v-form>
+    </v-card-text>
+    <v-card-actions class="pa-3">
+      <v-btn flat color="teal">Registrarse</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="blue lighten-1">Ingresar</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import {mixinForm} from '../mixins'
+export default {
+  props: ["title", "error"],
+  mixins:[mixinForm]
+};
+</script>
+
+<style>
+.title {
+  color: rgba(66, 165, 245, 1);
+}
+</style>
---
+++ src/scenes/Login/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-layout wrap align-center justify-center>
+            <v-flex xs12 sm6>
+                <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
+            </v-flex>
+            <v-flex xs12 sm4>
+                <slot name="form"></slot>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import BaseForm from './components/baseForm.vue'
+export default {
+   
+}
+</script>
+
+<style>
+
+</style>
