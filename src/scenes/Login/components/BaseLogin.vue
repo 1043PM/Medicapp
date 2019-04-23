69a900bd2a3f0e52f20c69b4a1db674f7c7a4d0f
Alex <alejandro.sanchez.queb@gmail.com>
2019-04-23 00:12:08 -0500

    added password field

--- src/scenes/Login/components/BaseLogin.vue
+++ src/scenes/Login/components/BaseLogin.vue
@@ -3,25 +3,56 @@
     <v-card-title class="title">{{title}}</v-card-title>
     <v-card-text>
       Bienvenido a la pagina de Servicios Medicos Profesionales para el transporte
+      {{text}}
       <v-alert color="red" v-show="Boolean(error)">{{error}}</v-alert>
-      <v-form>    
+      <v-form>
         <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-
+        <v-text-field
+          :append-icon="show ? 'visibility' : 'visibility_off'"          
+          :type="show ? 'text' : 'password'"
+          name="password"
+          label="Password"   
+          v-model="password"       
+          @click:append="show = !show"
+        ></v-text-field>
+        <div class="my-2">
+            <router-link class="caption" to="/" >No tiene una cuenta?</router-link>                
+        </div>
       </v-form>
     </v-card-text>
     <v-card-actions class="pa-3">
-      <v-btn flat color="teal">Registrarse</v-btn>
+      <v-btn flat color="teal" :loading="loadingForm">Registrarse</v-btn>
       <v-spacer></v-spacer>
-      <v-btn color="blue lighten-1">Ingresar</v-btn>
+      <v-btn color="blue lighten-1" :loading="loadingForm">Ingresar</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import {mixinForm} from '../mixins'
+import { mixinForm } from "../mixins";
 export default {
-  props: ["title", "error"],
-  mixins:[mixinForm]
+  props: ["title", "error", "text"],
+  mixins: [mixinForm],
+  data() {
+    return {
+      show: false,      
+      password: ''
+    };
+  },
+  methods:{
+      login(){
+        this.loadingForm = true
+        this.$store.dispatch('login',{
+            email: this.email,
+            password: this.password
+        }).then((user) =>{
+            this.loadingForm = false
+        }).catch((error)=>{
+            this.loadingForm = false
+            this.error = error
+        })
+      }
+  }
 };
 </script>
 
